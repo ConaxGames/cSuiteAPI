e09e881849b61c27083ae910172fb5e98dad3f91
bowpy <57394670+oliverbowp@users.noreply.github.com>
2023-11-27 15:45:16 +0000

    Add `player` to `CorePlayerPermissionRefreshEvent`

--- src/main/java/com/conaxgames/api/events/player/CorePlayerPermissionRefreshEvent.java
+++ src/main/java/com/conaxgames/api/events/player/CorePlayerPermissionRefreshEvent.java
@@ -3,16 +3,21 @@ package com.conaxgames.api.events.player;
 import com.conaxgames.api.events.CoreEvent;
 import com.conaxgames.api.ICorePlayer;
 import lombok.Getter;
+import org.bukkit.entity.Player;
+
+import javax.annotation.Nullable;
 
 @Getter
 public class CorePlayerPermissionRefreshEvent extends CoreEvent {
 
     private final ICorePlayer corePlayer;
+    @Nullable private final Player player;
     private final String reason;
     private final Long currentSystemLong;
 
-    public CorePlayerPermissionRefreshEvent(ICorePlayer corePlayer, String reason) {
+    public CorePlayerPermissionRefreshEvent(ICorePlayer corePlayer, @Nullable Player player, String reason) {
         this.corePlayer = corePlayer;
+        this.player = player;
         this.reason = reason;
         this.currentSystemLong = System.currentTimeMillis();
     }
