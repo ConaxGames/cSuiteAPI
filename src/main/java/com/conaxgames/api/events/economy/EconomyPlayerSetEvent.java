10ad654293a6baed2469388e0145bd29f930b568
bowpy <57394670+oliverbowp@users.noreply.github.com>
2023-12-02 21:21:37 +0000

    Economy events

---
+++ src/main/java/com/conaxgames/api/events/economy/EconomyPlayerSetEvent.java
@@ -0,0 +1,16 @@
+package com.conaxgames.api.events.economy;
+
+import com.conaxgames.api.events.CoreEvent;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.bukkit.OfflinePlayer;
+
+@Getter
+@RequiredArgsConstructor
+public class EconomyPlayerSetEvent extends CoreEvent {
+
+    private final OfflinePlayer player;
+    private final double amount;
+    private final boolean success;
+
+}
