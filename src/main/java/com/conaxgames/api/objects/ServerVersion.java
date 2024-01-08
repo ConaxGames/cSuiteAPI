702e1cbe8d831e83bedbacbd4a4dfee4e52f5f47
Brendan Callanan <brendan.business@outlook.com>
2024-01-08 13:37:36 +0100

    Added v1_20_R3 Support

--- src/main/java/com/conaxgames/api/objects/ServerVersion.java
+++ src/main/java/com/conaxgames/api/objects/ServerVersion.java
@@ -14,7 +14,8 @@ public enum ServerVersion {
     v1_19_R3,
     v1_19_R4,
     v1_20_R1,
-    v1_20_R2;
+    v1_20_R2,
+    v1_20_R3;
 
     public boolean after(ServerVersion serverVersion) {
         return this.ordinal() > serverVersion.ordinal();
