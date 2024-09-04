d189d72a556a965d7b13108af1f80460d40241e6
Brendan Callanan <30944704+bennycallanan@users.noreply.github.com>
2024-09-04 23:08:05 +0100

    javadocs for CorePluginAPI

--- src/main/java/com/conaxgames/CorePluginAPI.java
+++ src/main/java/com/conaxgames/CorePluginAPI.java
@@ -3,27 +3,64 @@ package com.conaxgames;
 import com.conaxgames.api.ICorePlugin;
 import lombok.Getter;
 
+/**
+ * The {@code CorePluginAPI} class provides static access to the core plugin instance
+ * and utility methods for interacting with it.
+ *
+ * <p>This class also includes methods to send debug information and check if the
+ * core plugin has been registered.</p>
+ *
+ * <p>It is designed to work as a singleton, where only one instance of the {@code CorePluginAPI}
+ * can exist at any time.</p>
+ */
 @Getter
 public final class CorePluginAPI {
 
+    /**
+     * The instance of the core plugin used by this API.
+     */
     private static ICorePlugin plugin;
 
+    /**
+     * The singleton instance of the {@code CorePluginAPI}.
+     */
     public static CorePluginAPI instance;
 
+    /**
+     * Constructs a new {@code CorePluginAPI} with the specified {@code ICorePlugin}.
+     *
+     * @param corePlugin the core plugin instance to be set.
+     */
     public CorePluginAPI(ICorePlugin corePlugin) {
         instance = this;
         plugin = corePlugin;
     }
 
+    /**
+     * Returns the core plugin instance associated with this API.
+     *
+     * @return the {@code ICorePlugin} instance.
+     */
     @SuppressWarnings("unused")
     public static ICorePlugin getPlugin() {
         return plugin;
     }
 
+    /**
+     * Sends a debug message with the specified action and information.
+     *
+     * @param action the action being debugged.
+     * @param info additional information related to the action.
+     */
     public static void sendDebug(String action, String info) {
         plugin.sendCritical(action, info);
     }
 
+    /**
+     * Checks if the {@code CorePluginAPI} has been registered.
+     *
+     * @return {@code true} if the API is registered, {@code false} otherwise.
+     */
     public static boolean isRegistered() {
         return instance != null;
     }
