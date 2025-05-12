8504c09d6607ab61e4cd875a32fbec96241bde73
Brendan Callanan <brendan.business@outlook.com>
2025-05-12 20:41:43 +0100

    feat: added guild wages methods

--- src/main/java/com/conaxgames/api/interfaces/IGuild.java
+++ src/main/java/com/conaxgames/api/interfaces/IGuild.java
@@ -60,4 +60,35 @@ public interface IGuild {
      * @param amount The new bank balance
      */
     void setBankBalance(double amount);
+    
+    /**
+     * Get the current guild wage amount per member
+     * @return The current wage amount
+     */
+    default double getWageAmount() {
+        return 0.0;
+    }
+    
+    /**
+     * Set the amount of money each member will receive as daily wage
+     * @param amount The amount to set as wage
+     */
+    default void setWageAmount(double amount) {
+    }
+    
+    /**
+     * Get the timestamp of the last wage payment
+     * @return The timestamp in milliseconds
+     */
+    default long getLastWagePayment() {
+        return 0L;
+    }
+    
+    /**
+     * Calculate if there is enough money in the bank to pay wages
+     * @return True if there are sufficient funds
+     */
+    default boolean hasEnoughFundsForWages() {
+        return true;
+    }
 }
