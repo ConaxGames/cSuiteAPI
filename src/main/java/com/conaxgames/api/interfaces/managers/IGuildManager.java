ba013c3a3f57c6b280ebe626e5c0853d1957e145
bennycallanan <brendan.finance@outlook.com>
2026-01-24 20:46:52 +0000

    feat: canBypassGuildPvP method

--- src/main/java/com/conaxgames/api/interfaces/managers/IGuildManager.java
+++ src/main/java/com/conaxgames/api/interfaces/managers/IGuildManager.java
@@ -4,6 +4,8 @@ import com.conaxgames.api.ICorePlayer;
 import com.conaxgames.api.interfaces.IGuild;
 import com.conaxgames.api.objects.GuildRank;
 
+import java.util.UUID;
+
 public interface IGuildManager {
 
     boolean isRankedAtLeast(ICorePlayer corePlayer, GuildRank guildRank);
@@ -12,5 +14,8 @@ public interface IGuildManager {
 
     IGuild getGuildByGuildID(int id);
 
+    boolean canBypassGuildPvP(UUID player1, UUID player2);
+
+    void setGuildPvPBypass(UUID player1, UUID player2, boolean bypass);
 
 }
