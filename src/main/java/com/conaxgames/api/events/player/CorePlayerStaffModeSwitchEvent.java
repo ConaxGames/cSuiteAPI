a538555e4de91efa62669cb46db03eda2f3c6a72
bennycallanan <brendan.finance@outlook.com>
2026-03-31 23:17:21 +0100

    feat: CorePlayerStaffModeSwitchEvent

---
+++ src/main/java/com/conaxgames/api/events/player/CorePlayerStaffModeSwitchEvent.java
@@ -0,0 +1,19 @@
+package com.conaxgames.api.events.player;
+
+import com.conaxgames.api.events.CoreEvent;
+import com.conaxgames.api.ICorePlayer;
+import lombok.Getter;
+
+@Getter
+public class CorePlayerStaffModeSwitchEvent extends CoreEvent {
+
+    private final ICorePlayer corePlayer;
+    private final boolean staffMode;
+    private final long timestamp;
+
+    public CorePlayerStaffModeSwitchEvent(ICorePlayer corePlayer, boolean staffMode) {
+        this.corePlayer = corePlayer;
+        this.staffMode = staffMode;
+        this.timestamp = System.currentTimeMillis();
+    }
+}
