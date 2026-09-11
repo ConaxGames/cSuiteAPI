4be07ddd31a3293b8f3081c7ad09d3cebf62ae6c
Brendan Callanan <30944704+bennycallanan@users.noreply.github.com>
2026-09-11 18:19:35 +0100

    feat: added support for grant creation/removal using API

--- src/main/java/com/conaxgames/api/managers/IGrantManager.java
+++ src/main/java/com/conaxgames/api/managers/IGrantManager.java
@@ -1,6 +1,9 @@
 package com.conaxgames.api.managers;
 
 import com.conaxgames.api.interfaces.IGrant;
+import com.conaxgames.api.interfaces.IRank;
+import com.conaxgames.api.interfaces.IScope;
+import org.bukkit.command.CommandSender;
 
 import java.util.List;
 import java.util.UUID;
@@ -9,5 +12,9 @@ import java.util.concurrent.CompletableFuture;
 public interface IGrantManager {
 
     CompletableFuture<List<IGrant>> loadIGrants(UUID uuid);
+
+    CompletableFuture<IGrant> createGrant(CommandSender sender, UUID target, IRank rank, IScope scope, long duration, String reason);
+
+    CompletableFuture<Boolean> removeGrant(CommandSender sender, IGrant grant, String reason);
 
 }
